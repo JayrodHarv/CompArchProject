fb94def949e4c1ad3a56e45bbdda6cc17ae942d2
Jared Harvey <jared.harvey10@gmail.com>
2026-05-06 12:43:36 -0500

    added project part 3 files and instantiated new components in sisc. Renamed all instances to the names the autograder wants.

---
+++ Part3/ctrl.v
@@ -0,0 +1,184 @@
+// ECE:3350 SISC computer project
+// finite state machine
+
+`timescale 1ns/100ps
+
+module ctrl (clk, rst_f, opcode, mm, stat, rf_we, alu_op, wb_sel, br_sel, pc_rst, pc_write, pc_sel, ir_load, mm_sel, dm_we);
+
+	input clk, rst_f;
+	input [3:0] opcode, mm, stat;
+	output reg rf_we, wb_sel;
+	output reg [3:0] alu_op;
+	output reg br_sel;
+	output pc_rst;
+	output reg pc_write;
+	output reg pc_sel;
+	output reg ir_load;
+	output reg mm_sel;
+	output reg dm_we;
+
+	assign pc_rst = ~rst_f; // pc_rst is the inverse of rst_f
+  
+	// state parameter declarations
+	
+	parameter start0 = 0, start1 = 1, fetch = 2, decode = 3, execute = 4, mem = 5, writeback = 6;
+	
+	// opcode parameter declarations
+	
+	parameter NOOP = 0, REG_OP = 1, REG_IM = 2, SWAP = 3, BRA = 4, BRR = 5, BNE = 6, BNR = 7;
+	parameter JPA = 8, JPR = 9, LOD = 10, STR = 11, CALL = 12, RET = 13, HLT = 15;
+		
+	// addressing modes
+	
+	parameter AM_IMM = 8;
+
+	// state register and next state signal
+	
+	reg [2:0]  present_state, next_state;
+
+	// initial procedure to initialize the present state to 'start0'.
+
+	initial
+		present_state = start0;
+
+	/* Procedure that progresses the fsm to the next state on the positive edge of 
+		the clock, OR resets the state to 'start1' on the negative edge of rst_f. 
+		Notice that the computer is reset when rst_f is low, not high. */
+
+	always @(posedge clk, negedge rst_f)
+	begin
+		if (rst_f == 1'b0)
+			present_state <= start1;
+		else
+			present_state <= next_state;
+	end
+	
+	/* The following combinational procedure determines the next state of the fsm. */
+
+	always @(present_state, rst_f)
+	begin
+		case(present_state)
+			start0:     next_state = start1;
+			start1:     if (rst_f == 1'b0) next_state = start1;
+									else next_state = fetch;
+			fetch:      next_state = decode;
+			decode:     next_state = execute;
+			execute:    next_state = mem;
+			mem:        next_state = writeback;
+			writeback:  next_state = fetch;
+			default:    next_state = start1;
+		endcase
+	end
+
+	always @(present_state, opcode)
+	begin
+
+		// Defaults (set everything to zero)
+		ir_load  = 0;
+		pc_write = 0;
+		pc_sel   = 0;
+		br_sel   = 0;
+		rf_we    = 0;
+		alu_op   = 4'b0000;
+		wb_sel   = 0;
+
+		case (present_state)
+
+			fetch:
+			begin
+				ir_load  = 1;
+				pc_write = 1;
+				pc_sel   = 0;
+			end
+
+			decode:
+			begin
+				case (opcode)
+					// Branch Instructions
+					BRA:
+					if (|(mm & stat))
+					begin
+						pc_sel = 1;
+						pc_write = 1;
+						br_sel = 1;
+					end
+
+					BRR:
+					if (|(mm & stat))
+					begin
+						pc_sel = 1;
+						pc_write = 1;
+						br_sel = 0;
+					end
+
+					BNE:
+					if (!(|(mm & stat)))
+					begin
+						pc_sel = 1;
+						pc_write = 1;
+						br_sel = 1;
+					end
+
+					BNR:
+					if (!(|(mm & stat)))
+					begin
+						pc_sel = 1;
+						pc_write = 1;
+						br_sel = 0;
+					end
+					
+					default:
+					begin
+						pc_sel = 0;
+						pc_write = 0;
+						br_sel = 0;
+					end
+				endcase
+			end
+
+			mem:
+			begin
+				case (opcode)
+					REG_OP:			alu_op = 4'b0000;
+					REG_IM:			alu_op = 4'b0010;
+					default:		alu_op = 4'b0000;
+				endcase
+			end
+
+			execute:
+			begin
+				case (opcode)
+					NOOP:     alu_op = 4'b0000;
+					REG_OP:   alu_op = 4'b0001;
+					REG_IM:   alu_op = 4'b0011;
+
+					default:  alu_op = 4'b0000;
+				endcase
+			end
+
+			writeback:
+			begin
+				case (opcode)
+					REG_OP,REG_IM:	rf_we = 1'b1;
+					default:
+					begin 
+						rf_we = 1'b0;
+						wb_sel = 1'b0;
+						alu_op = 4'b0000;
+					end
+				endcase
+			end
+		endcase
+	end
+
+	// Halt on HLT instruction
+	
+	always @ (opcode)
+	begin
+		if (opcode == HLT)
+		begin 
+		#5 $display ("Halt."); //Delay 5 ns so $monitor will print the halt instruction
+		$stop;
+		end
+	end
+endmodule
---
+++ Part3/sisc.v
@@ -0,0 +1,191 @@
+// ECE:3350 SISC processor project
+// main SISC module, part 1
+
+`timescale 1ns / 100ps
+
+module sisc (
+	clk,
+	rst_f
+);
+
+	input clk, rst_f;
+
+	// ---------------------------------
+	// declare all internal wires here
+	// ---------------------------------
+	wire [3:0] 	alu_op;     // ALU operation code from control unit to ALU
+	wire        wb_sel;     // writeback select from control unit to writeback mux
+	wire        rf_we;      // register file write enable from control unit to register file
+	wire [3:0]  stat_en;	// status register enable from control unit to status register
+	wire [3:0]  stat;       // Wire connecting the status register to the control circuit
+
+	wire [31:0] rsa, rsb;   // outputs from register file
+	wire [3:0]  cc;         // condition code from alu to status register
+	wire [31:0] alu_out;    // output from alu to writeback mux and status register
+	wire [31:0] wb_data;    // output from writeback mux to register file
+
+	// ---------------------------------
+	// PART 2 WIRES
+	// ---------------------------------
+	wire [15:0] br_addr;
+	wire [15:0] pc_out;
+	wire [31:0]	instr_out;
+	wire [31:0]	instr;
+
+	// control signals
+	wire 		br_sel;
+	wire 		pc_rst;
+	wire 		pc_write;
+	wire 		pc_sel;
+	wire		ir_load;
+
+	// ---------------------------------
+	// PART 3 WIRES
+	// ---------------------------------
+	wire [15:0] mux16_out;
+	wire 		dm_we;
+	wire 		mm_sel;
+	wire [31:0] read_data;
+
+	// ---------------------------------
+	// instruction decoding
+	// ---------------------------------
+	wire [3:0]	opcode      = instr[31:28];    	// opcode
+	wire [3:0] 	mff			= instr[27:24];		// Mode / Flag Field (Multi-purpose field)
+	wire [3:0] 	rd			= instr[23:20];		// Write Register
+	wire [3:0] 	rs			= instr[19:16];		// Source Register A
+	wire [3:0]	rt			= instr[15:12];		// Source Register B
+
+	wire [3:0] funct		= instr[3:0];		// ALU function code
+
+	wire [15:0]	imm			= instr[15:0];		// Immediate Value
+	wire [15:0] target		= instr[15:0];		// Memory Address 	  	
+
+	// ---------------------------------
+	// component instantiation goes here
+	// ---------------------------------
+
+	// control unit (CTRL)
+	ctrl u1 (
+		clk,
+		rst_f,
+		opcode,
+		mff,
+		stat,
+		rf_we,
+		alu_op,
+		wb_sel,
+		br_sel,
+		pc_rst,
+		pc_write,
+		pc_sel,
+		ir_load,
+		mm_sel,
+		dm_we
+	);
+
+	// register file
+	rf u2 (
+		clk,
+		rs,
+		rt,
+		rd,
+		wb_data,
+		rf_we,
+		rsa,
+		rsb
+	);
+
+	// Arithmetic Logic Unit (ALU)
+	alu u3 (
+		clk,
+		rsa,
+		rsb,
+		imm,
+		cc[3],  // carry in from status register
+		alu_op,
+		mff,
+		alu_out,
+		cc,
+		stat_en
+	);
+
+	// writeback mux
+	mux32 u5 (
+		alu_out,
+		read_data,
+		wb_sel,
+		wb_data
+	);
+
+	// status register (STATREG)
+	statreg u6 (
+		clk,
+		cc,
+		stat_en,
+		stat
+	);
+
+	// Branch Address Calculator (BR)
+	br u7 (
+		pc_out,
+		target,
+		br_sel,
+		br_addr
+	);
+
+	// Instruction Memory (IM)
+	im u8 (
+		pc_out,
+		instr_out
+	);
+
+	// Instruction Register (IR)
+	ir u9 (
+		clk,
+		ir_load,
+		instr_out,
+		instr
+	);
+
+	// Program Counter (PC)
+	pc u10 (
+		clk,
+		br_addr,
+		pc_sel,
+		pc_write,
+		pc_rst,
+		pc_out
+	);
+
+	dm u11 (
+		mux16_out,
+		mux16_out,
+		rsb,
+		dm_we,
+		read_data
+	);
+
+	mux16 u13 (
+		alu_out,
+		imm,
+		mux16_out
+	);
+
+	initial begin
+		$monitor("Time = %0d IR = %h PC = %h R1 = %h R2 = %h R3 = %h R4 = %h R5 = %h ALU_OP = %h BR_SEL = %b PC_WRITE = %b PC_SEL = %b",
+			$time, instr, pc_out, u2.ram_array[1], u2.ram_array[2], u2.ram_array[3], u2.ram_array[4], u2.ram_array[5], alu_op, br_sel, pc_write, pc_sel
+		);
+//     $monitor(
+//         "| Time=%0d | \n\
+// | PC=%h | PC_SEL=%b | PC_WRITE=%b | STAT=%b | BR_SEL=%b | BR_ADDR=%h | \n\
+// | IR=%h | IR_LOAD=%b | RS=%h | RT=%h | RD=%h | MFF=%b | OPCODE=%b | \n\
+// | R1=%h | R2=%h | R3=%h | R4=%h | R5=%h | RF_WE=%b | \n\
+// | ALU_OP=%b | \n",
+//           $time, pc_out, pc_sel, pc_write, stat, br_sel, br_addr,
+//           instr, ir_load, rsa, rsb, rd, mff, opcode,
+//           rf0.ram_array[1], rf0.ram_array[2], rf0.ram_array[3], rf0.ram_array[4], rf0.ram_array[5], rf_we,
+//           alu_op
+//     );
+end
+endmodule
